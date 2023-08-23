27b632c92294c91f9e318b5905c1183d4149ed41
isadkang <akungamef543@gmail.com>
2023-08-23 11:30:43 +0700

    new answer: quiz2.js

--- part-6-loop/forloop.js
+++ part-6-loop/forloop.js
@@ -7,5 +7,5 @@ for (let i = 0; i < names.length; i++){
 console.log(' ')
 
 for (let i = names.length - 1; i >= 0; i--){
-    console.log(`After reverse : ${names[i].join()}`)
+    console.log(`After reverse : ${names[i]}`)
 }
