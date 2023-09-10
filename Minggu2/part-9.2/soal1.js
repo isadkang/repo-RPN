2cda62e470b4abbbb0e67a177616712622d1f229
isadkang <akungamef543@gmail.com>
2023-09-10 18:47:03 +0700

    fix human error

--- Minggu2/part-9.2/soal1.js
+++ Minggu2/part-9.2/soal1.js
@@ -3,17 +3,21 @@ console.log('Soal 1 - Indentifikasi prima 1 or 0');
 //cek apakah angka yang dikirim adalah angka prima atau bukan?
 //cek google bagi yang ga tau apa itu angka prima
 function angkaPrima(angka) {
-    // you can only write your code here!
-    if (angka % 2 != 0) {
-        return `${angka} = ${true} Prima`
-    } else {
-        return `${angka} = ${false} !Prima`
+    // Math.sqrt() = fungsi bawaan js untuk ngitung akar
+    for (let i = 2; i <= Math.sqrt(angka); i++) {
+        if (angka % i === 0) {
+            return `${angka} false`
+        }
     }
+    
+    return `${angka} true`
 }
 
 // TEST CASES
-console.log(angkaPrima(2)); // false
 console.log(angkaPrima(3)); // true
+console.log(angkaPrima(9)); // false
+console.log(angkaPrima(15)); // false
+console.log(angkaPrima(21)); // false
 console.log(angkaPrima(7)); // true
 console.log(angkaPrima(6)); // false
 console.log(angkaPrima(23)); // true
